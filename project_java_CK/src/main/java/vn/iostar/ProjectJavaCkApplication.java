aba8e19d92f0f5effbe6f4ed21c261cf460ee0d2
a01689355133a <a01654464356@gmail.com>
2024-11-30 18:39:57 +0700

    first commit

--- project_java_CK/src/main/java/vn/iostar/ProjectJavaCkApplication.java
+++ project_java_CK/src/main/java/vn/iostar/ProjectJavaCkApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectJavaCkApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectJavaCkApplication.class, args);
 	}
-
 }
