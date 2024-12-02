b0771936bc42e45f8895893068debfdf62a027d5
Vo Van Nam <vovannam220304@gmail.com>
2024-12-02 14:50:29 +0700

    layout_thymeleaf

---
+++ project_java_CK/src/main/java/vn/iostar/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package vn.iostar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+        .csrf().disable() // Tắt CSRF nếu không cần thiết
+        .authorizeRequests()
+            .anyRequest().permitAll() // Cho phép tất cả các request mà không cần xác thực
+        .and()
+        .formLogin().disable() // Tắt trang login mặc định của Spring Security
+        .httpBasic().disable() // Tắt xác thực HTTP Basic
+        .logout()
+            .logoutUrl("/logout") // Đảm bảo đường dẫn logout đúng
+            .logoutSuccessUrl("/") // Chuyển hướng về trang chủ sau khi đăng xuất (hoặc trang bạn muốn)
+            .invalidateHttpSession(true) // Đảm bảo xóa session khi logout
+            .deleteCookies("JSESSIONID"); // Xóa cookie session khi logout
+
+    return http.build();
+    }
+}
---
+++ project_java_CK/src/main/java/vn/iostar/controllers/WHomeController.java
@@ -0,0 +1,16 @@
+package vn.iostar.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class WHomeController {
+	
+	@GetMapping("/")
+	public String index()
+	{
+		return "admin/home-admin";
+	}
+
+}
