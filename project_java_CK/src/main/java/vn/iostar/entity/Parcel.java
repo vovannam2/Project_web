8e3a5ea848caa2e4a4dbeb3c55e9f70a1539f312
TramTieuMi <157827317+TramTieuMi@users.noreply.github.com>
2024-12-16 00:13:51 +0700

    them lading code

--- project_java_CK/src/main/java/vn/iostar/entity/Parcel.java
+++ project_java_CK/src/main/java/vn/iostar/entity/Parcel.java
@@ -4,18 +4,27 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 
-
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Getter
+@Setter
 @Table(name = "parcels")
 public class Parcel {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Integer parcelId;
+    // mã vận đơn: -> know user id:
+    @Column(name = "ladingCode", unique = true, nullable = false)
+    private String ladingCode;
+
+    @PrePersist
+    private void generateLadingCode() {
+        this.ladingCode = "LDC-" + UUID.randomUUID().toString().substring(0, 8).toUpperCase();
+    }
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
@@ -42,14 +51,25 @@ public class Parcel {
     private User shipper;
 
     private Float weight;
+    
+    private String image;
 
     private String status;
 
     private String note;
-
+    
+    //Thêm chi phí vận chuyển
+    @JoinColumn(name = "shipping_fee")
+    private int shippingFee;
+    
     @ManyToOne
     @JoinColumn(name = "shipping_type_id")
     private ShippingType shippingType;
+    
+    @ManyToOne
+    @JoinColumn(name = "parcel_type_id", nullable = false)
+    private ParcelType parcelType;
+
 
     private LocalDateTime createDate;
 
